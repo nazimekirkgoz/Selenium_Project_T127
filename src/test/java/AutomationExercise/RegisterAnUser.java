f77f0884d8311922ecfffd1004d6b123b662b41f
Nazime <kirkgoznazime@gmail.com>
2023-11-29 16:04:16 +0100

    29112023

--- src/test/java/AutomationExercise/RegisterAnUser.java
+++ src/test/java/AutomationExercise/RegisterAnUser.java
@@ -1,16 +1,18 @@
 package AutomationExercise;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
-import utilities.Before_After;
+import utilities.ReusableMethods;
+import utilities.TestBase;
 
-public class RegisterAnUser extends Before_After {
-    /*
-   1. Launch browser
+public class RegisterAnUser extends TestBase {
+
+               /*
+1. Launch browser
 2. Navigate to url 'http://automationexercise.com'
 3. Verify that home page is visible successfully
 4. Click on 'Signup / Login' button
@@ -28,74 +30,61 @@ public class RegisterAnUser extends Before_After {
 16. Verify that 'Logged in as username' is visible
 17. Click 'Delete Account' button
 18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
-     */
+         */
+
+
+
     @Test
-    public void registerUser() throws InterruptedException {
-        // 2. Navigate to url 'http://automationexercise.com'
-        driver.get("http://automationexercise.com");
+    public void test01 () {
+
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("https://automationexercise.com/");
 
         //3. Verify that home page is visible successfully
-
-        //  4. Click on 'Signup / Login' button
-        driver.findElement(By.xpath("//a[@href='/login']")).click();
+        String expectedUrl = "https://automationexercise.com/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl, actualUrl);
+        //4. Click on 'Signup / Login' button
+        driver.findElement(By.xpath("//i[@class='fa fa-lock']")).click();
 
         //5. Verify 'New User Signup!' is visible
+        WebElement newusertitle = driver.findElement(By.xpath("//div[@class='signup-form']"));
+        Assert.assertTrue(newusertitle.isDisplayed());
 
-        //6. Enter name and email address
-        WebElement signUpName = driver.findElement(By.xpath("//*[@data-qa='signup-name']"));
+        //6-Enter name and email address
+        WebElement name = driver.findElement(By.xpath("//*[@data-qa='signup-name']"));
         Actions actions = new Actions(driver);
-        actions.click(signUpName)
-                .sendKeys("Ecrin")
+        actions.click(name)
+                .sendKeys("selin")
                 .sendKeys(Keys.TAB)
-                .sendKeys("ecrin@gmail.com")
+                .sendKeys("selin6@gmail.com")
                 .perform();
 
+
         //7. Click 'Signup' button
-        driver.findElement(By.xpath("//*[@data-qa='signup-button']")).submit();
-        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@data-qa='signup-button']")).click();
 
-        //8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        // 8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        WebElement account = driver.findElement(By.xpath("(//h2[@class='title text-center'])[1]"));
+        Assert.assertTrue(account.isDisplayed());
+
         //9. Fill details: Title, Name, Email, Password, Date of birth
-
-        //10. Select checkbox 'Sign up for our newsletter!'
-        driver.findElement(By.id("name")).sendKeys(Keys.PAGE_DOWN);
-        driver.findElement(By.id("newsletter")).click();
-
-        //11. Select checkbox 'Receive special offers from our partners!'
-        driver.findElement(By.id("optin")).click();
-
-        //12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
-        WebElement firstName = driver.findElement(By.xpath("//*[@data-qa='first_name']"));
-        actions.click(firstName)
-                .sendKeys("Ecrin")
+        WebElement title = driver.findElement(By.xpath("//div[@class='radio-inline'][2]")); //titlet
+        Actions actions1 = new Actions(driver);
+        actions1.click(title)
+                .sendKeys(Keys.PAGE_DOWN)
                 .sendKeys(Keys.TAB)
-                .sendKeys("Yilmaz")
+
                 .sendKeys(Keys.TAB)
-                .sendKeys("Efficy")
+
+                .sendKeys("2344545")
                 .sendKeys(Keys.TAB)
-                .sendKeys("12. street")
+                .sendKeys("5")
                 .sendKeys(Keys.TAB)
-                .sendKeys("next to the rond point").perform();
-
-        WebElement country = driver.findElement(By.id("country"));
-        Select countryElement = new Select(country);
-        countryElement.selectByVisibleText("Canada");
-
-        WebElement state = driver.findElement(By.id("state"));
-        actions.click(state)
-                .sendKeys("California")
+                .sendKeys("May")
                 .sendKeys(Keys.TAB)
-                .sendKeys("Los Angeles")
+                .sendKeys("1993")
                 .sendKeys(Keys.TAB)
-                .sendKeys("90001")
-                .sendKeys(Keys.TAB)
-                .sendKeys("12345678").perform();
-
-        //13. Click 'Create Account button'
-        driver.findElement(By.xpath("//*[@data-qa='create-account']")).submit();
-
-        // Verify that 'ACCOUNT CREATED!' is visible
-
-
-
+                .perform();
+        ReusableMethods.bekle(3);
 }}
